b1a6bf182690722a6227c1f8d08caab493c8df6d
Divino Neto <divinoneto93@gmail.com>
2024-11-25 21:11:11 -0300

    remove app

--- frontend/app.js
+++
@@ -1,9 +0,0 @@
-fetch('/api/helloworlds')
-    .then(response => response.json())
-    .then(data => {
-        const greetingsDiv = document.getElementById('greetings');
-        data.forEach(greeting => {
-            greetingsDiv.innerHTML += `<p>${greeting.language}: ${greeting.message}</p>`;
-        });
-    })
-    .catch(error => console.error('Error:', error));
